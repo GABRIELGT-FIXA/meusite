8f438b3e54a171ca6ae42688ab16df5ea776546b
GABRIELGT-FIXA <84719443+GABRIELGT-FIXA@users.noreply.github.com>
2023-05-31 13:50:55 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const healer = document.querySelector('.header.container');
+
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('activate');
+    mobile_menu.classList.toggle('activate');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('activate');
+		mobile_menu.classList.toggle('activate');
+	});
+});
